f49976ac22eef56c59c17c830d483bf2f29b217b
Nathan Watkins <43277379+Nazza1996@users.noreply.github.com>
2024-09-18 14:48:51 +0100

    Update main.js
    Creates and deletes the icons folder upon opening and closing the app to save storage space

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const { getApps } = require('./getActiveApps.js');
+const fs = require('fs');
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -17,11 +18,18 @@ function createWindow() {
     win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow, fs.mkdirSync('./icons'));
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit();
+        // First, delete the folder, then quit the app
+        fs.rm('./icons', { recursive: true, force: true }, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Folder deleted');
+            app.quit(); // Quit after the folder is deleted
+        });
     }
 });
 
