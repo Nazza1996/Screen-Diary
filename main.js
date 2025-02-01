7bd8a20bcff803b8fd1a0f8c8d51f91799912ea8
Nathan Watkins <nathan.wtkns@gmail.com>
2025-02-01 17:02:41 +0000

    Formatted documents

--- main.js
+++ main.js
@@ -1,185 +1,218 @@
-const { app, BrowserWindow, ipcMain, Menu, Tray, contentTracing, dialog } = require('electron'); // Import Electron modules
-const path = require('path'); // Import Node.js path module
-const { getApps, saveData, loadData, ifImageExists } = require('./getActiveApps.js'); // Import functions from getActiveApps.js
-const { toggleRunOnStartup, toggleStartMinimised, initializeSettings, toggleCloseToTray,
-    exportSettings, importSettings, clearIconCache, factoryReset, clearHistory
-} = require('./settingsScripts.js'); // Import functions from settingsScripts.js
-const Store = require('electron-store'); // Import electron-store module
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Menu,
+  Tray,
+  contentTracing,
+  dialog,
+} = require("electron"); // Import Electron modules
+const path = require("path"); // Import Node.js path module
+const {
+  getApps,
+  saveData,
+  loadData,
+  ifImageExists,
+} = require("./getActiveApps.js"); // Import functions from getActiveApps.js
+const {
+  toggleRunOnStartup,
+  toggleStartMinimised,
+  initializeSettings,
+  toggleCloseToTray,
+  exportSettings,
+  importSettings,
+  clearIconCache,
+  factoryReset,
+  clearHistory,
+} = require("./settingsScripts.js"); // Import functions from settingsScripts.js
+const Store = require("electron-store"); // Import electron-store module
 
 let win = null; // Initialize the main window variable
 
 // Ensure only a single instance of the application is running
 const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
-    app.quit(); // Quit if another instance is already running
+  app.quit(); // Quit if another instance is already running
 } else {
-    app.on('second-instance', () => {
-        if (win) {
-            if (win.isMinimized()) {
-                win.restore(); // Restore the window if it is minimized
-            }
-            win.show(); // Show the window
-            win.focus(); // Focus the window
-        }
-    });
+  app.on("second-instance", () => {
+    if (win) {
+      if (win.isMinimized()) {
+        win.restore(); // Restore the window if it is minimized
+      }
+      win.show(); // Show the window
+      win.focus(); // Focus the window
+    }
+  });
 }
 
 // Function to create the main application window
 function createWindow() {
-    win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'), // Preload script
-            contextIsolation: true, // Enable context isolation
-            enableRemoteModule: false, // Disable remote module
-            nodeIntegration: false, // Disable Node.js integration
-            devTools: false // Disable developer tools
-        },
-        titleBarStyle: 'hidden', // Hide the title bar
-        titleBarOverlay: {
-            color: '#ececec', // Set the title bar overlay color
-            height: 40 // Set the title bar overlay height
-        }
-    });
+  win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"), // Preload script
+      contextIsolation: true, // Enable context isolation
+      enableRemoteModule: false, // Disable remote module
+      nodeIntegration: false, // Disable Node.js integration
+      devTools: false, // Disable developer tools
+    },
+    titleBarStyle: "hidden", // Hide the title bar
+    titleBarOverlay: {
+      color: "#ececec", // Set the title bar overlay color
+      height: 40, // Set the title bar overlay height
+    },
+  });
 
-    // Create a system tray icon
-    let tray = new Tray(path.join(__dirname, "/assets/icon.png"));
-    const contextMenu = Menu.buildFromTemplate([
-        {label: 'Show Screen Diary', click: () => {
-            win.show(); // Show the window
-            win.focus(); // Focus the window
-        }},
-        {type: 'separator'},
-        {label: 'Quit', click: () => {
+  // Create a system tray icon
+  let tray = new Tray(path.join(__dirname, "/assets/icon.png"));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show Screen Diary",
+      click: () => {
+        win.show(); // Show the window
+        win.focus(); // Focus the window
+      },
+    },
+    { type: "separator" },
+    {
+      label: "Quit",
+      click: () => {
+        dialog
+          .showMessageBox(win, {
+            type: "question",
+            buttons: [
+              "Quit Screen Diary",
+              "Run Screen Diary in the Background",
+              "Cancel",
+            ],
+            title: "Quit Screen Diary",
+            message: "Are you sure you want to quit Screen Diary?",
+            detail:
+              "If you quit Screen Diary, you will not be able to track your usage. Running Screen Diary in the background will continue to track your usage.\n\nYour data will be saved before quitting.",
+            detailType: "info",
+            icon: path.join(__dirname, "/assets/icon.png"),
+          })
+          .then((result) => {
+            if (result.response === 0) {
+              win.webContents.send("request-variable-from-renderer"); // Request variable from renderer
+              win.hide();
+              setTimeout(() => {
+                saveData(currentAppData); // Save the current app data
+                win.destroy(); // Destroy the window
+                app.quit(); // Quit the application
+              }, 1000); // Delay to ensure data is saved
+            } else if (result.response === 1) {
+              const win = BrowserWindow.getAllWindows()[0]; // Get the focused window
+              win.hide(); // Hide the window
+            } else {
+              return; // Do nothing if the user cancels
+            }
+          });
+      },
+    },
+  ]);
 
-            dialog.showMessageBox(win, {
-                type: 'question',
-                buttons: ['Quit Screen Diary', 'Run Screen Diary in the Background', 'Cancel'],
-                title: 'Quit Screen Diary',
-                message: 'Are you sure you want to quit Screen Diary?',
-                detail: 'If you quit Screen Diary, you will not be able to track your usage. Running Screen Diary in the background will continue to track your usage.\n\nYour data will be saved before quitting.',
-                detailType: 'info',
-                icon: path.join(__dirname, '/assets/icon.png')
-            }).then((result) => {
-                if (result.response === 0) {
-                    win.webContents.send('request-variable-from-renderer'); // Request variable from renderer
-                    win.hide();
-                    setTimeout(() => {
-                        saveData(currentAppData); // Save the current app data
-                        win.destroy(); // Destroy the window
-                        app.quit(); // Quit the application
-                    }, 1000); // Delay to ensure data is saved
-                } else if (result.response === 1) {
-                    const win = BrowserWindow.getAllWindows()[0]; // Get the focused window
-                    win.hide(); // Hide the window
-                } else {
-                    return; // Do nothing if the user cancels
-                }
-            });
-        }}
-    ]);
+  tray.setToolTip("Screen Diary"); // Set the tray tooltip
+  tray.setContextMenu(contextMenu); // Set the tray context menu
+  tray.on("click", () => {
+    win.show(); // Show the window on tray icon click
+    win.focus(); // Focus the window on tray icon click
+  });
 
-    tray.setToolTip('Screen Diary'); // Set the tray tooltip
-    tray.setContextMenu(contextMenu); // Set the tray context menu
-    tray.on('click', () => {
-        win.show(); // Show the window on tray icon click
-        win.focus(); // Focus the window on tray icon click
-    });
+  win.setMenuBarVisibility(false); // Hide the menu bar
 
-    win.setMenuBarVisibility(false); // Hide the menu bar
-    
-    win.loadFile('./index.html'); // Load the main HTML file
+  win.loadFile("./index.html"); // Load the main HTML file
 
-    // Handle window close event for non-macOS platforms
-    win.on('close', () => {
-        if (process.platform !== 'darwin') {
-            win.webContents.send('request-variable-from-renderer'); // Request variable from renderer
-        }
-    });
+  // Handle window close event for non-macOS platforms
+  win.on("close", () => {
+    if (process.platform !== "darwin") {
+      win.webContents.send("request-variable-from-renderer"); // Request variable from renderer
+    }
+  });
 }
 
 const store = new Store(); // Initialize electron-store
 
 // When the application is ready, create the main window
 app.whenReady().then(() => {
-    createWindow();
-    if (store.get('startMinimised') == true) {
-        win.hide(); // Hide the window if startMinimised setting is true
-    }
+  createWindow();
+  if (store.get("startMinimised") == true) {
+    win.hide(); // Hide the window if startMinimised setting is true
+  }
 });
 
 // Quit the application when all windows are closed, except on macOS
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit(); // Quit the application
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit(); // Quit the application
+  }
 });
 
 // Handle IPC requests from the renderer process
-ipcMain.handle('get-active-apps', async () => {
-    return await getApps(); // Return the active apps
+ipcMain.handle("get-active-apps", async () => {
+  return await getApps(); // Return the active apps
 });
 
-ipcMain.handle('save-data-with-data', (event, processData) => {
-    return saveData(processData); // Save the provided data
+ipcMain.handle("save-data-with-data", (event, processData) => {
+  return saveData(processData); // Save the provided data
 });
 
-ipcMain.handle('load-data', () => {
-    return loadData(); // Load the saved data
+ipcMain.handle("load-data", () => {
+  return loadData(); // Load the saved data
 });
 
-ipcMain.handle('image-exists', (event, path) => {
-    return ifImageExists(path); // Check if the image exists
+ipcMain.handle("image-exists", (event, path) => {
+  return ifImageExists(path); // Check if the image exists
 });
 
 let currentAppData = null; // Initialize the current app data variable
-ipcMain.on('send-variable-to-main', (event, variable) => {
-    currentAppData = variable; // Update the current app data
+ipcMain.on("send-variable-to-main", (event, variable) => {
+  currentAppData = variable; // Update the current app data
 });
 
-ipcMain.handle('toggle-run-on-startup', async () => {
-    return await toggleRunOnStartup(); // Toggle the run on startup setting
+ipcMain.handle("toggle-run-on-startup", async () => {
+  return await toggleRunOnStartup(); // Toggle the run on startup setting
 });
 
-ipcMain.handle('toggle-start-minimised', () => {
-    toggleStartMinimised(); // Toggle the start minimized setting
+ipcMain.handle("toggle-start-minimised", () => {
+  toggleStartMinimised(); // Toggle the start minimized setting
 });
 
-ipcMain.handle('toggle-close-to-tray', () => {
-    toggleCloseToTray(); // Toggle the close to tray setting
+ipcMain.handle("toggle-close-to-tray", () => {
+  toggleCloseToTray(); // Toggle the close to tray setting
 });
 
-ipcMain.handle('get-settings', async () => {
-    return new Promise ((resolve, reject) => {
-        resolve(initializeSettings()); // Initialize and return the settings
-    });
+ipcMain.handle("get-settings", async () => {
+  return new Promise((resolve, reject) => {
+    resolve(initializeSettings()); // Initialize and return the settings
+  });
 });
 
-ipcMain.handle('set-accent-colour', async (event, colour) => {
-    store.set('accentColour', colour); // Set the accent colour
+ipcMain.handle("set-accent-colour", async (event, colour) => {
+  store.set("accentColour", colour); // Set the accent colour
 });
 
-ipcMain.handle('get-store-value', async (event, key) => {
-    return store.get(key); // Get the value from the store
+ipcMain.handle("get-store-value", async (event, key) => {
+  return store.get(key); // Get the value from the store
 });
 
-ipcMain.handle('export-settings', () => {
-    return exportSettings(); // Export the settings
+ipcMain.handle("export-settings", () => {
+  return exportSettings(); // Export the settings
 });
 
-ipcMain.handle('import-settings', () => {
-    return importSettings(); // Import the settings
+ipcMain.handle("import-settings", () => {
+  return importSettings(); // Import the settings
 });
 
-ipcMain.handle('clear-icon-cache', () => {
-    return clearIconCache(); // Export the settings
+ipcMain.handle("clear-icon-cache", () => {
+  return clearIconCache(); // Export the settings
 });
 
-ipcMain.handle('factory-reset', () => {
-    return factoryReset(); // Factory reset the application
+ipcMain.handle("factory-reset", () => {
+  return factoryReset(); // Factory reset the application
 });
 
-ipcMain.handle('clear-history', () => {
-    return clearHistory(); // Clear the history
+ipcMain.handle("clear-history", () => {
+  return clearHistory(); // Clear the history
 });
--- settingsScripts.js
+++ settingsScripts.js
@@ -1,137 +1,174 @@
-const path = require('path'); // Import Node.js path module
-const os = require('os'); // Import Node.js os module
-const ws = require('windows-shortcuts'); // Import windows-shortcuts module
-const fs = require('fs'); // Import Node.js file system module
-const Store = require('electron-store'); // Import electron-store module
-const { app, BrowserWindow, dialog } = require('electron');
+const path = require("path"); // Import Node.js path module
+const os = require("os"); // Import Node.js os module
+const ws = require("windows-shortcuts"); // Import windows-shortcuts module
+const fs = require("fs"); // Import Node.js file system module
+const Store = require("electron-store"); // Import electron-store module
+const { app, BrowserWindow, dialog } = require("electron");
 
 const store = new Store(); // Initialize electron-store
 
 // Function to initialize settings with default values if not already set
 function initializeSettings() {
-    if (store.get('runOnStartup') == undefined) {
-        store.set('runOnStartup', false); // Set default value for runOnStartup
-    }
+  if (store.get("runOnStartup") == undefined) {
+    store.set("runOnStartup", false); // Set default value for runOnStartup
+  }
 
-    if (store.get('startMinimised') == undefined) {
-        store.set('startMinimised', false); // Set default value for startMinimised
-    }
+  if (store.get("startMinimised") == undefined) {
+    store.set("startMinimised", false); // Set default value for startMinimised
+  }
 
-    if (store.get('closeToTray') == undefined) {
-        store.set('closeToTray', false); // Set default value for closeToTray
-    }
+  if (store.get("closeToTray") == undefined) {
+    store.set("closeToTray", false); // Set default value for closeToTray
+  }
 
-    // Return the current settings
-    return {
-        runOnStartup: store.get('runOnStartup'),
-        startMinimised: store.get('startMinimised'),
-        closeToTray: store.get('closeToTray')
-    };
+  // Return the current settings
+  return {
+    runOnStartup: store.get("runOnStartup"),
+    startMinimised: store.get("startMinimised"),
+    closeToTray: store.get("closeToTray"),
+  };
 }
 
 // Function to toggle the run on startup setting
 async function toggleRunOnStartup() {
-    const startUpFolder = path.join(os.homedir(), 'AppData', 'Roaming', 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup'); // Path to the startup folder
-    const exePath = path.join(__dirname, '../../ScreenDiary.exe'); // Path to the executable
-    const targetPath = path.join(startUpFolder, 'ScreenDiary.lnk'); // Path to the shortcut
+  const startUpFolder = path.join(
+    os.homedir(),
+    "AppData",
+    "Roaming",
+    "Microsoft",
+    "Windows",
+    "Start Menu",
+    "Programs",
+    "Startup"
+  ); // Path to the startup folder
+  const exePath = path.join(__dirname, "../../ScreenDiary.exe"); // Path to the executable
+  const targetPath = path.join(startUpFolder, "ScreenDiary.lnk"); // Path to the shortcut
 
-    if (!fs.existsSync(targetPath)) {
-        // Create a shortcut if it doesn't exist
-        ws.create(targetPath, exePath, (err) => {
-            if (err) {
-                console.error('Failed to create shortcut: ', err); // Log any errors
-            }
-        });
-        store.set('runOnStartup', true); // Update the setting to true
-    } else {
-        // Remove the shortcut if it exists
-        fs.rmSync(targetPath);
-        store.set('runOnStartup', false); // Update the setting to false
-    }
+  if (!fs.existsSync(targetPath)) {
+    // Create a shortcut if it doesn't exist
+    ws.create(targetPath, exePath, (err) => {
+      if (err) {
+        console.error("Failed to create shortcut: ", err); // Log any errors
+      }
+    });
+    store.set("runOnStartup", true); // Update the setting to true
+  } else {
+    // Remove the shortcut if it exists
+    fs.rmSync(targetPath);
+    store.set("runOnStartup", false); // Update the setting to false
+  }
 }
 
 // Function to toggle the start minimized setting
 function toggleStartMinimised() {
-    store.set('startMinimised', !store.get('startMinimised')); // Toggle the start minimized setting
+  store.set("startMinimised", !store.get("startMinimised")); // Toggle the start minimized setting
 }
 
 // Function to toggle the close to tray setting
 function toggleCloseToTray() {
-    store.set('closeToTray', !store.get('closeToTray')); // Toggle the close to tray setting
+  store.set("closeToTray", !store.get("closeToTray")); // Toggle the close to tray setting
 }
 
 app.whenReady().then(() => {
-    setTimeout(() => {
-        const win = BrowserWindow.getAllWindows()[0]; // Get the focused window
-        win.on('close', function (event) {
-            if (store.get('closeToTray') == true) {
-                event.preventDefault(); // Prevent the default window close action
-                win.hide(); // Hide the window
-            }
-        });
-    }, 1000);
+  setTimeout(() => {
+    const win = BrowserWindow.getAllWindows()[0]; // Get the focused window
+    win.on("close", function (event) {
+      if (store.get("closeToTray") == true) {
+        event.preventDefault(); // Prevent the default window close action
+        win.hide(); // Hide the window
+      }
+    });
+  }, 1000);
 });
 
 function exportSettings() {
-    dialog.showSaveDialog({
-        title: 'Export Settings',
-        defaultPath: path.join(os.homedir(), `Screen Diary Settings ${new Date().toISOString().replaceAll(':', '-')}.json`),
-        filters: [{ name: 'JSON', extensions: ['json'] }],
-    }).then(result => {
-        if (!result.canceled) {
-            fs.writeFileSync(result.filePath, JSON.stringify(store.store, null, 4));
-        }
-    }).catch(err => {
-        console.log(err);
-    });   
+  dialog
+    .showSaveDialog({
+      title: "Export Settings",
+      defaultPath: path.join(
+        os.homedir(),
+        `Screen Diary Settings ${new Date()
+          .toISOString()
+          .replaceAll(":", "-")}.json`
+      ),
+      filters: [{ name: "JSON", extensions: ["json"] }],
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        fs.writeFileSync(result.filePath, JSON.stringify(store.store, null, 4));
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function importSettings() {
-    dialog.showOpenDialog({
-        title: 'Import Settings',
-        filters: [{ name: 'JSON', extensions: ['json'] }],
-        properties: ['openFile'],
-    }).then(result => {
-        if (!result.canceled) {
-            const data = JSON.parse(fs.readFileSync(result.filePaths[0]));
-            // Check if data contains the required settings
-            if (data.hasOwnProperty('runOnStartup') && data.hasOwnProperty('startMinimised') && data.hasOwnProperty('closeToTray')) {
-                store.store = data;
-                app.relaunch(); // Reload the window
-                app.exit(); // Exit the application to apply changes and relaunch
-            } else {
-                dialog.showErrorBox('Invalid Settings', 'The selected file does not contain valid settings for this application.');
-            }
+  dialog
+    .showOpenDialog({
+      title: "Import Settings",
+      filters: [{ name: "JSON", extensions: ["json"] }],
+      properties: ["openFile"],
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        const data = JSON.parse(fs.readFileSync(result.filePaths[0]));
+        // Check if data contains the required settings
+        if (
+          data.hasOwnProperty("runOnStartup") &&
+          data.hasOwnProperty("startMinimised") &&
+          data.hasOwnProperty("closeToTray")
+        ) {
+          store.store = data;
+          app.relaunch(); // Reload the window
+          app.exit(); // Exit the application to apply changes and relaunch
+        } else {
+          dialog.showErrorBox(
+            "Invalid Settings",
+            "The selected file does not contain valid settings for this application."
+          );
         }
-    }).catch(err => {
-        console.log(err);
-    });  
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function clearIconCache() {
-    const iconPath = path.join(app.getPath('userData'), '/Icons');
-    if (fs.existsSync(iconPath)) {
-        fs.rmSync(iconPath, { recursive: true});
-    }
+  const iconPath = path.join(app.getPath("userData"), "/Icons");
+  if (fs.existsSync(iconPath)) {
+    fs.rmSync(iconPath, { recursive: true });
+  }
 }
 
 function factoryReset() {
-    if (store.get('runOnStartup') == true) {
-        toggleRunOnStartup(); // Toggle the run on startup setting
-    }
-    store.clear(); // Clear all settings
-    app.relaunch(); // Reload the window
-    app.exit(); // Exit the application to apply changes and relaunch
+  if (store.get("runOnStartup") == true) {
+    toggleRunOnStartup(); // Toggle the run on startup setting
+  }
+  store.clear(); // Clear all settings
+  app.relaunch(); // Reload the window
+  app.exit(); // Exit the application to apply changes and relaunch
 }
 
 function clearHistory() {
-    const historyPath = path.join(app.getPath('userData'), '/Save Data');
-    if (fs.existsSync(historyPath)) {
-        fs.rmSync(historyPath, { recursive: true});
-    }
-    app.relaunch(); // Reload the window
-    app.exit(); // Exit the application to apply changes and relaunch
+  const historyPath = path.join(app.getPath("userData"), "/Save Data");
+  if (fs.existsSync(historyPath)) {
+    fs.rmSync(historyPath, { recursive: true });
+  }
+  app.relaunch(); // Reload the window
+  app.exit(); // Exit the application to apply changes and relaunch
 }
 
 // Export the functions for use in other modules
-module.exports = { toggleRunOnStartup, initializeSettings, toggleStartMinimised, toggleCloseToTray, exportSettings, importSettings, clearIconCache, factoryReset, clearHistory };
+module.exports = {
+  toggleRunOnStartup,
+  initializeSettings,
+  toggleStartMinimised,
+  toggleCloseToTray,
+  exportSettings,
+  importSettings,
+  clearIconCache,
+  factoryReset,
+  clearHistory,
+};
